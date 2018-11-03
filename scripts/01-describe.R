74ea3016a1fc75078b923f41352423562078c740
Oleksandr Dergai <o.dergai@gmail.com>
2018-11-03 23:20:07 +0100

    added code to analyse Street, Alley, LotShape

--- scripts/01-describe.R
+++ scripts/01-describe.R
@@ -1,7 +1,7 @@
 # load packages
 #-------------------------------------------------------------------------------
 
-packages <- c("here", "dplyr", "funModeling", "ggplot2", "magrittr", "tibble", "gtools")
+packages <- c("here", "dplyr", "funModeling", "ggplot2", "magrittr", "tibble", "gtools", "minerva")
 sapply(packages, library,
        character.only = TRUE, logical.return = TRUE, quietly = TRUE)
 
@@ -9,7 +9,7 @@ sapply(packages, library,
 #-------------------------------------------------------------------------------
 
 houses <- read.csv(
-    file = here("data", "raw", "train.csv"),
+    file = here("funky","data", "raw", "train.csv"),
     header = TRUE,
     sep = ",",
     stringsAsFactors = FALSE
@@ -88,3 +88,50 @@ houses %>%
     geom_point() +
     xlim(0, 50000) +
     stat_smooth(method = "lm", formula = y ~ x)
+
+# Street
+#-------------------------------------------------------------------------------
+street_summary <- describe(houses$Street)
+street_summary
+
+
+## 99.6% of streets are paved.. maybe we can get riid of this parameter
+
+
+#Alley
+#-------------------------------------------------------------------------------
+#Grvl	Gravel = 50
+#Pave	Paved = 41
+#NA 	No alley access = 1369
+
+describe(houses$Alley)
+# I would suggest to rename NA to NO_alley or so...
+houses$Alley[is.na(houses$Alley)] <- "no_access"
+describe(houses$Alley)
+
+houses %>% select(Alley) %>% mutate(replace(houses$Alley, list = which(is.na(houses$Alley)), values = "No_alley" )) %>% pull %>% as.data.frame() %>%
+    ggplot() + geom_boxplot(aes(x = as.factor(Value), y = SalePrice))
+alley <- houses %>% select(Alley) %>% mutate(replace(houses$Alley, list = which(is.na(houses$Alley)), values = "No_alley" )) %>% pull %>% as.factor()
+houses %>% ggplot() + geom_boxplot(aes(x = alley, y = SalePrice) )
+aov_fit <- aov(SalePrice ~ alley, data = houses)
+summary(aov_fit)                        
+TukeyHSD(aov_fit)
+## looks like there is not much of difference between No_allay and Paved in terms of explaining SalePrice, one may think to merge them
+## but their is significant difference between Gravel and the rest.
+
+#LotShape
+#-------------------------------------------------------------------------------
+describe(houses$LotShape)
+houses %>% ggplot() + geom_boxplot(aes(x = LotShape, y = SalePrice))
+lot_price_fit <- aov(SalePrice ~ LotShape, data = houses)
+TukeyHSD(lot_price_fit) 
+
+## IR3 encompasses 7% and is not different from  IR1, IR2 and IR3 are not different, let up merge IR1+IR3 to get IR1, IR2, Reg 
+LotShape_mut <- houses %>% select(LotShape) %>% mutate(LotShape = replace(LotShape, LotShape == "IR3", "IR1" )) %>% select(LotShape) %>% pull %>%as.factor()
+houses %>% ggplot() + geom_boxplot(aes(x = LotShape_mut, y = SalePrice)) 
+aov_fit_lot_shape <- aov(SalePrice ~ LotShape_mut, data = houses)
+summary(aov_fit_lot_shape)                        
+TukeyHSD(aov_fit_lot_shape) ## all comparisons have very low pvalues...
+
+## I think it is not a bad idea to merge IR1 with IR3 and keep the rest.
+
